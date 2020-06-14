e06de3ae2cb397231119b3daf86ba5aea5502d1c
Angel Flores <cyberangel1000@gmail.com>
2020-06-14 15:59:55 -0500

    Se agregó un nuevo middleware que se usará más adelante para poder agregar los datos faltantes en la biografía de un usuario

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -5,7 +5,10 @@ from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
  
-
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+#Models
+from users.models import Profile
 
 def login_view(request):
     """ Login view. """
@@ -31,3 +34,33 @@ def logout_view(request):
     logout(request)
     # Redirect to a success page.        
     return redirect('login')
+
+def signup(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users\signup.html', {'error': 'Password confirmation does not match'})
+        try:
+            user = User.objects.create_user(username=username, password=passwd, email=email)
+        except IntegrityError:
+            return render(request, 'users\signup.html', {'error': 'This user already exists'})
+        
+        
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.save()
+        profile =  Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+
+    return render(request, 'users/signup.html')
+
+def update_profile(request):
+    return render(request, 'users/update_profile.html')
