fb7cedb29fd24850e95c8db133278e9a530b5ccb
Angel Flores <cyberangel1000@gmail.com>
2020-10-19 23:39:03 +0000

    ELiminados archivos de env

--- .env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\angel fa\documents\onlinelearning\platzi\django\.env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
